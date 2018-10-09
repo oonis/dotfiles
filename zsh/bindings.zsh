d66f8fca98a5655887a1ec3ff6fa3f2cf77c4cd0
Sam Macaluso <sam@macalu.so>
2018-10-09 13:31:27 -0400

    trying zsh

---
+++ zsh/bindings.zsh
@@ -0,0 +1,7 @@
+export KEYTIMEOUT=1
+
+# Control
+bindkey "^f" beginning-of-line
+bindkey "^g" end-of-line
+bindkey "^n" forward-word
+bindkey "^b" backward-word
---
+++ zsh/zshrc.zsh
@@ -0,0 +1,4 @@
+source ~/.dotfiles/zsh/alias.zsh
+source ~/.dotfiles/zsh/bindings.zsh
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
