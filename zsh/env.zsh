13445ea2a560db5726c291518b0eed1154978b39
Sam Macaluso <sam@macalu.so>
2018-10-25 10:32:33 -0400

    Added Go

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -7,9 +7,8 @@ export SUDO_EDITOR='nvim'
 # PATH
 export PATH=$PATH:~/.dotfiles/bin
 export PATH=$PATH:~/.cargo/bin # Rust
-#export GOPATH="$(go env GOPATH)" # Go
-#export PATH="$PATH:$GOPATH/bin" # Go
-#export PATH=$PATH:~/.npm-global/bin # npm globally installed
+export GOPATH="$(go env GOPATH)" # Go
+export PATH="$PATH:$GOPATH/bin" # Go
 
 . ~/.nix-profile/etc/profile.d/nix.sh # Nix working
 
