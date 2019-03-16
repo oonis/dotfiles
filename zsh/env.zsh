922c9ef65dcce54ef7de312f9f5f210a76a49ce8
Sam Macaluso <sam@macalu.so>
2019-03-16 10:16:02 -0400

    Update

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -5,10 +5,9 @@ export EDITOR='nvim'
 export SUDO_EDITOR='nvim'
 
 # PATH
-export PATH=$PATH:~/.dotfiles/bin
 export PATH=$PATH:~/.cargo/bin # Rust
 export PATH="$PATH:$GOPATH/bin" # Go
-export GOPATH=$HOME/Dev/go # Go TODO: This shouldn't be hardcoded
+export GOPATH=$HOME/Dev/go
 
 . ~/.nix-profile/etc/profile.d/nix.sh # Nix working
 
