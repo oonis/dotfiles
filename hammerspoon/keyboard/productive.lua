2ed18fbaa5fab2447ceb56d242803fd5319df82a
Sam Macaluso <sam@macalu.so>
2019-04-28 21:42:39 -0400

    update

--- hammerspoon/keyboard/productive.lua
+++
@@ -1,6 +0,0 @@
-local productiveMappings = require('keyboard.productive-bindings-defaults')
-
-local modifiers = productiveMappings.modifiers
-local showHelp  = productiveMappings.showHelp
-local trigger   = productiveMappings.trigger
-local mappings  = productiveMappings.mappings
