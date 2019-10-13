b0a2a83dfcd98d94f25d983856850bb80103181a
Sam Macaluso <sam@macalu.so>
2019-10-12 21:59:09 -0400

    Updated GPG key and added brew file

---
+++ Brewfile
@@ -0,0 +1,12 @@
+tap "homebrew/cask"
+tap "homebrew/cask-fonts"
+
+brew "neovim"
+brew "getantibody/tap/antibody"
+brew "git"
+brew "go"
+brew "exa"
+
+cask "font-fira-code"
+cask "iterm2"
+cask "karabiner"
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -9,6 +9,4 @@ export GOPATH=$HOME/Dev/go
 export PATH=$PATH:~/.cargo/bin # Rust
 export PATH="$PATH:$GOPATH/bin" # Go
 
-. ~/.nix-profile/etc/profile.d/nix.sh # Nix working
-
 typeset -U PATH # Remove duplicates in $PATH
